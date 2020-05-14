a3326b0032277a8782d01794fd82b7d71b1040e5
Raphael Cássio de Souza <rapha.cassio@gmail.com>
2020-05-13 21:20:44 -0300

    feat: added delete transaction funcionality

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,9 +1,9 @@
-import { getCustomRepository, TransactionRepository } from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 import { Router } from 'express';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -45,7 +45,13 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute({ id });
+
+  return response.status(204).send();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
